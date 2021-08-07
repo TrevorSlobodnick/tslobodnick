cd738e382bd9e82a2316581a7a41f9517b5ae7d3
Trevor Slobodnick <trevorslobodnick@gmail.com>
2021-08-06 23:53:53 -0400

    Updated all the links to be accurate, some of the github repositories dont exist yet but they will be created shortly, otherwise all links will now work for every project.

--- Main/JS/Constants.js
+++ Main/JS/Constants.js
@@ -6,9 +6,9 @@ const projects = {
         "title" : "Museum Tracker For Animal Crossing New Horizons",
         "description" : "I always wanted an easy way to track my museum progress in Animal Crossing New Horizons, so I built this app. It is used to log which bugs, fossils, art, fish, and other sea creatures you have in your museum. It also makes it easy to find the creatures you dont have in your museum, so you can start searching for them.",
         "links" : {
-            "github" : "https://www.tslobodnick.ca/ComingSoon/",
-            "info" : "https://www.tslobodnick.ca/ComingSoon/",
-            "web" : "https://www.tslobodnick.ca/MuseumTracker/"
+            "github" : "https://www.tslobodnick.ca/ACNHMuseumTracker",
+            "info" : "https://www.tslobodnick.ca/ProjectInfo/MuseumTracker",
+            "web" : "https://www.tslobodnick.ca/MuseumTracker"
         }
     },
     "Pokemon" : {
@@ -16,17 +16,17 @@ const projects = {
         "description" : "This is an app that I made for myself to help manage/track my pokemon card collection.",
         "links" : {
             "github" : "https://github.com/TrevorSlobodnick/PokemonCardCollection_v2",
-            "info" : "https://www.tslobodnick.ca/ComingSoon/",
-            "web" : "https://www.tslobodnick.ca/PokemonCardTracker/"
+            "info" : "https://www.tslobodnick.ca/ProjectInfo/PokemonCardCollection",
+            "web" : "https://www.tslobodnick.ca/PokemonCardTracker"
         }
     },
     "Voltorb" : {
         "title" : "Voltorb Flip Solver",
         "description" : "This app generates all the possible solutions of a given board from the game \"Voltorb Flip\". Voltorb Flip is a game in Pokemon Heartgold/Soulsilver that is similar to Minesweeper. The goal of the game is to uncover all of the 2 and 3 tiles on a given board and move up to higher levels, but if the tile you select is a Voltorb/bomb, you lose.",
         "links" : {
-            "github" : "https://www.tslobodnick.ca/ComingSoon/",
-            "info" : "https://www.tslobodnick.ca/ComingSoon/",
-            "web" : "https://www.tslobodnick.ca/VoltorbFlip/"
+            "github" : "https://www.tslobodnick.ca/ComingSoon/VoltorbFlip",
+            "info" : "https://www.tslobodnick.ca/ProjectInfo/VoltorbFlip",
+            "web" : "https://www.tslobodnick.ca/VoltorbFlip"
         }
     },
     "Blimeys" : {
@@ -34,26 +34,26 @@ const projects = {
         "description" : "This is a redesign of a small british store called Blimeys",
         "links" : {
             "github" : "https://github.com/TrevorSlobodnick/BlimeysRedesign",
-            "info" : "https://www.tslobodnick.ca/ComingSoon/",
-            "web" : "https://www.tslobodnick.ca/Blimeys/"
+            "info" : "https://www.tslobodnick.ca/ProjectInfo/Blimeys",
+            "web" : "https://www.tslobodnick.ca/Blimeys"
         }
     },
     "AmongUs" : {
         "title" : "Among Us Fansite",
         "description" : "This is a site for the game among us that has a bunch of different info about the game and developers, and also features a chatroom.",
         "links" : {
-            "github" : "https://github.com/TrevorSlobodnick/BlimeysRedesign",
-            "info" : "https://www.tslobodnick.ca/ComingSoon/",
-            "web" : "https://www.tslobodnick.ca/AmongUsWeb/"
+            "github" : "https://github.com/TrevorSlobodnick/AmongUsWeb",
+            "info" : "https://www.tslobodnick.ca/ProjectInfo/AmongUsWeb",
+            "web" : "https://www.tslobodnick.ca/AmongUsWeb"
         }
     },
     "Kibble" : {
         "title" : "Kibble Up",
         "description" : "This is a site that contains all the important kibble information for the game \"Ark: Survival Evolved\". It is used to get info on what kind of kibble a dino prefers, what kind of kibble you can make with different dino eggs, as well as a list of all the kibble recipes.",
         "links" : {
-            "github" : "https://www.tslobodnick.ca/ComingSoon/",
-            "info" : "https://www.tslobodnick.ca/ComingSoon/",
-            "web" : "https://www.tslobodnick.ca/KibbleUp/"
+            "github" : "https://www.tslobodnick.ca/KibbleUp",
+            "info" : "https://www.tslobodnick.ca/ProjectInfo/KibbleUp",
+            "web" : "https://www.tslobodnick.ca/KibbleUp"
         }
     }
 }
